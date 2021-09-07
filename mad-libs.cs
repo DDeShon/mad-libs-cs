76b9636bdc040e56b2ec8198a1c6250a0ffe9a1d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-09-07 09:16:36 -0500

    updated README.md

---
+++ mad-libs.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Libs_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init Variables
+            // Get User Input
+            // Init Story
+            // Print Story
+        }
+    }
+}
