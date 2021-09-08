3c26160c63f78018663766098aeda5dacbdb89ed
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-09-08 09:54:18 -0500

    Initializing Variables

--- mad-libs.cs
+++ mad-libs.cs
@@ -12,18 +12,20 @@ namespace Mad_Libs_CS
         {
             // Init Variables
             string theMatrix, system, neo, enemy, inside, save, unplugged, fight;
+            string[] profession = new string[4], adj = new string[2];
+
             // Get User Input
 
             // Init Story
             string madlibsStory =
             $"{theMatrix} is a {system}, {Neo}. That {system} is our {enemy}. " +
             $"But when you're {inside}, you look around, what do you see? " +
-            $"Businessmen, teachers, lawyers, carpenters. The very minds " +
+            $"{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds " +
             $"of the people we are trying to {save}. But until we do, " +
             $"these people are still a part of that {system} and that makes " +
             $"them our {enemy}. You have to understand, most of these people " +
-            $"are not ready to be {unplugged}. And many of them are so inured, " +
-            $"so hopelessly dependent on the {system}, that they will {fight} to protect it.";
+            $"are not ready to be {unplugged}. And many of them are so {adj[0]}, " +
+            $"so hopelessly {adj[1]} on the {system}, that they will {fight} to protect it.";
 
             // Print Story
             Console.WriteLine(madlibsStory);
